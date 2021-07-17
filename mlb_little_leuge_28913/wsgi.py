857ee29d9129589affcebbe331cbd9d4e15110c8
Crowdbotics <team@crowdbotics.com>
2021-07-17 22:53:42 +0000

    Update models

--- mlb_little_leuge_28913/wsgi.py
+++ mlb_little_leuge_28913/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mlb_little_leuge_28913.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mlb_little_leuge_28913.settings")
 
 application = get_wsgi_application()
